f4c225ee717e524ce416d4a18d83425cc583ea1f
Giuseppe Zito <sha1-7380d6786446b2a2af7dbb0310b9de8281c37b4e@cern.ch>
2007-10-08 11:26:04 +0000

    drawPalette added

--- test/TrackerGeometryTest.cc
+++ test/TrackerGeometryTest.cc
@@ -58,7 +58,7 @@ TrackerGeometryTest::analyze( const edm::Event& iEvent, const edm::EventSetup& i
 {
   TrackerMap * tkMap = new TrackerMap( "GeomDetUnitId : move the mouse on the module and look at the second text line below");
   TrackerMap * tkMap1 = new TrackerMap( "Default rainbow palette test");
-  tkMap1->setPalette(2);
+  tkMap1->setPalette(1);
    using namespace edm;
 
    edm::LogInfo("TrackerGeometryTest")<< "Here I am";
@@ -220,10 +220,12 @@ if(subdet==1){moduleno=((*begin)->geographicalId().rawId() >>2)&0x3F; }
    }
 
     tkMap->print(true,0.,0.,"svgmap1");
+    tkMap->showPalette(true);
     tkMap->save(true,0.,0.,"svgmap1",1500,800,"png");
     tkMap->save(true,0.,0.,"svgmap1",1500,800,"jpg");
     tkMap->save(true,0.,0.,"svgmap1",1500,800,"pdf");
     tkMap->save(true,0.,0.,"svgmap1");//default format svg without Javascript
+    tkMap1->showPalette(true);
     tkMap1->print(true,0.,0.,"svgmap2");
 }
 
